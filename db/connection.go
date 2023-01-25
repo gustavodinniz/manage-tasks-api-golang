380b3128742fac811347c19c3b8f969fba5202bc
gustavodiniz <gustavodinniz@hotmail.com>
2023-01-25 14:16:33 -0300

    fix: change variable pass to password

--- db/connection.go
+++ db/connection.go
@@ -10,7 +10,7 @@ import (
 func OpenConnection() (*sql.DB, error) {
 	conf := configs.GetDB()
 
-	sc := fmt.Sprintf("host=%s port=%s user=%s pass=%s dbname=%s sslmode=disable",
+	sc := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
 		conf.Host, conf.Port, conf.User, conf.Pass, conf.Database)
 
 	conn, err := sql.Open("postgres", sc)
